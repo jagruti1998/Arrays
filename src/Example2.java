aeab63488ab2ba35ad2654a730bed9bde5308872
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-07-24 23:35:23 +0530

    new commit

---
+++ src/Example2.java
@@ -0,0 +1,23 @@
+//Trying to find the index of the element that has the value 7.
+public class Example2 {
+    public static void main(String[] args) {
+        int intArray[]=new int[7];
+
+        intArray[0]=20;
+        intArray[1]=35;
+        intArray[2]=-15;
+        intArray[3]=7;
+        intArray[4]=55;
+        intArray[5]=1;
+        intArray[6]=-22;
+
+        int index=-1;
+        for(int i=0;i<intArray.length;i++){ //linear time O(n)-retrieve without index
+            if(intArray[i]==7){
+                index=i;
+                break;
+            }
+        }
+        System.out.println("index= " +index);
+    }
+}
