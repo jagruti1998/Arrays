2639d1a7f7b2f0073d4c502e6afd2419e62f0c63
Jagruti Goswami <jagrutigoswami402@gmail.com>
2023-08-21 00:18:20 +0530

    missing number

---
+++ src/MissingNum_OP.java
@@ -0,0 +1,19 @@
+public class MissingNum_OP {
+        public int missingNumber(int[] nums) {
+            int n = nums.length;
+            int expectedSum = (n * (n + 1)) / 2;
+            int actualSum = 0;
+            for (int num : nums) {
+                actualSum += num;
+            }
+            return expectedSum - actualSum;
+        }
+
+        public static void main(String[] args) {
+            int[] nums = {0, 1, 3, 4, 5}; // Example array
+            MissingNum_OP mn=new MissingNum_OP();
+            int missingNumber = mn.missingNumber(nums);
+            System.out.println("Missing number: " + missingNumber);
+        }
+    }
+
